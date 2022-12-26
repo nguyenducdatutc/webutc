98d6a3c006ad423acadc6c97dab410a027699af8
nguyenducdatutc <nducdat1000@gmail.com>
2022-12-26 14:26:59 +0700

    update web

---
+++ function.js
@@ -0,0 +1,8 @@
+function Into_Main() {
+    window.location.href = "./../user/Login/index.html";
+}
+
+// ================================ Run Program ========================================
+document.getElementById("start_activity").onclick = function () {
+    Into_Main();
+}
